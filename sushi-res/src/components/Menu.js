1e09d57121525fa5211e42bc5307f348334c7689
codesforjenuel <codesforjenuel@gmail.com>
2022-02-22 15:14:23 -0800

    created divs and containers for the menu component

--- sushi-res/src/components/Menu.js
+++ sushi-res/src/components/Menu.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>Menu</div>
+    <div>
+<div className='menuContainer'>
+<div className='itemContainer'>
+    <h3>Beef Ramen</h3>
+    <img src='https://www.thespruceeats.com/thmb/82r1DrloeLBrNLd8tthGMDEq40o=/1500x1500/smart/filters:no_upscale()/GettyImages-174947093-13afc2cc6bb14d2982701e95ddfe4689.jpg' alt='beef ramen' className='itemImg' />
+</div>
+<div className='itemContainer'>
+<h3>Yakisoba</h3>
+<img src='https://www.justonecookbook.com/wp-content/uploads/2020/04/Yakisoba-5416-I-500x500.jpg' alt='beef ramen' className='itemImg' />
+</div>
+</div>
+</div>
   )
 }
